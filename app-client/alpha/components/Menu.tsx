71a40d94fb0d68499fec3125d8dc3ee478c779ed
Toby Sullivan <tobyjsullivan@gmail.com>
2019-11-09 15:57:39 -0800

    Add some menu items

--- app-client/alpha/components/Menu.tsx
+++ app-client/alpha/components/Menu.tsx
@@ -5,6 +5,15 @@ class Menu extends React.Component {
     return (
       <ul>
         <li><a href="#">Login/Sign Up</a></li>
+        <li><a href="#">Browse Popular Videos</a></li>
+        <li><a href="#">Upload a Video</a></li>
+        <li><a href="#">Ask a Question</a></li>
+        <li><a href="#">Browse Recent Videos</a></li>
+        <li><a href="#">Explore Trends</a></li>
+        <li><a href="#">Submit a Copyright Claim</a></li>
+        <li><a href="#">Browse Popular Locations</a></li>
+        <li><a href="#">Find a Nearby Bathroom</a></li>
+        <li><a href="#">Terms of Service</a></li>
       </ul>
     );
   }
